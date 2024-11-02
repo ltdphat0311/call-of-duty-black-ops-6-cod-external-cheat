f9954ef4fa533a6c8618b6ceff88bed975df7034
ltdphat0311 <phatb2111943@student.ctu.edu.vn>
2024-11-02 20:42:38 +0700

    update file

---
+++ steamoffsets.cpp
@@ -0,0 +1,43 @@
+namespace steam_offsets
+{
+        uint32_t o_item_base = 0xF9D4C60;
+ 
+        uint32_t o_item_pos = 0x1D0;
+        uint32_t o_item_size = 0x338;
+        uint32_t o_item_valid_1 = 0xC8;
+        uint32_t o_item_valid_2 = o_item_valid_1 + 0x2;
+ 
+        uint32_t o_visible_bit = 0x0;
+        uint32_t o_local_entity = 0x19F70;
+ 
+        uint32_t o_local_index = 0x5A2D8;
+        uint32_t o_local_index_pos = 0x2E4;
+ 
+        uint32_t o_refdef = 0x0;
+ 
+        uint32_t o_local_base = 0x16CFA920;
+        uint32_t o_camera_pos = 0x204;
+ 
+        uint32_t o_distribute = 0xDB19628;
+        uint32_t o_visible = 0xFFFF8009B2FBCAB0;
+        uint32_t o_visible_offset = 0xA88;
+ 
+        uint32_t o_visible_size = 0x1D4C;
+        uint32_t o_visible_pointer = 0x1F8;
+        uint32_t o_visible_cached_base = 0x78;
+        uint32_t o_visible_cmp_index = 0x78;
+        uint32_t o_visible_cmp_function = 0x0;
+ 
+        uint32_t o_bone_base = 0x15CFA0;
+        uint32_t o_bone_size = 0x188;
+ 
+        uint32_t o_game_mode = 0x139478A8;
+ 
+        uint32_t o_no_recoil = 0x32CF8;
+        uint32_t o_weapon_definition = 0x160B65D0;
+ 
+        uint32_t o_player_dead_1 = 0x0;
+        uint32_t o_player_dead_2 = 0x0;
+        uint32_t o_player_pos = 0xFF2AEF6;
+        uint32_t o_player_size = 0x1D70;
+}
